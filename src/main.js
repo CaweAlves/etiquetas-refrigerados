dfceb6f15d72514bf824b5ebee624cc0b7ad4081
Cawe541 <cawe.wewe04@gmail.com>
2024-12-26 21:52:03 -0300

    WIP: Electron

---
+++ src/main.js
@@ -0,0 +1,46 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { createRequire } from 'node:module'
+const require = createRequire(import.meta.url)
+const { BrowserWindow, app, ipcMain } = require('electron')
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.cjs'),
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  })
+
+  if (process.env.NODE_ENV === 'development') {
+    const rendererPort = process.argv[2]
+    mainWindow.loadURL(`http://localhost:${rendererPort}`)
+    mainWindow.webContents.openDevTools()
+  }
+  else {
+    mainWindow.loadFile(path.join(app.getAppPath(), './dist', 'index.html'))
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+ipcMain.on('message', (__event, message) => {
+  console.log(message)
+})
